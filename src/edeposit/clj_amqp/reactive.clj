46a940338cc638748c8b4e336aa9bcffb8799420
Jan Stavěl <stavel.jan@gmail.com>
2015-07-20 21:17:51 +0200

    added first method to emit messages from AMQP queue and make from them Observable

--- src/edeposit/clj_amqp/reactive.clj
+++ src/edeposit/clj_amqp/reactive.clj
@@ -1,5 +1,14 @@
 (ns edeposit.clj-amqp.reactive
   (:require    [rx.lang.clojure.core :as rx]
                )
+  (:import  [rx Observable]
+            [java.util.concurrent TimeUnit]
+            )
   )
 
+(defn make-queue-obs [fn interval time-unit]
+  (def my-time (Observable/interval interval time-unit))
+  (->> my-time 
+       (rx/map fn)
+      )
+  )
--- test/edeposit/clj_amqp/reactive_test.clj
+++ test/edeposit/clj_amqp/reactive_test.clj
@@ -1,7 +1,37 @@
 (ns edeposit.clj-amqp.reactive-test
   (:require [clojure.test :refer :all]
-            [edeposit.clj-amqp.reactive :refer :all]))
+            [edeposit.clj-amqp.reactive :as rea]
+            [rx.lang.clojure.core :as rx]
+            [rx.lang.clojure.blocking :as rxb]
+            )
+  (:import [java.util.concurrent TimeUnit]
+           [rx Observable]
+           )
+  )
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest observable-queue-test
+  (testing "new observable queue"
+    (def obs (rea/make-queue-obs (fn [val] 1) 10 TimeUnit/MILLISECONDS))
+    (def sum (->> obs
+                  (rx/take 10)
+                  (rx/reduce + 0)
+                  (rxb/last)
+                  )
+      )
+    (is (= sum 10)))
+  )
+
+(deftest just-non-nil-values
+  (testing "just non nil values queue emits"
+    (def obs (rea/make-queue-obs (fn [val] 1) 10 TimeUnit/MILLISECONDS))
+    )
+  )
+
+
+
+
+
+
+
+
+
