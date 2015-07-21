51468f7baf7d8768eb8d094c0afc4586e161224c
Jan Stavěl <stavel.jan@gmail.com>
2015-07-21 14:47:29 +0200

    jstavel/edeposit#454 doplnil jsem metodu pro nacitani zprav z amqp.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [io.reactivex/rxclojure "1.0.0"]
+                 [com.novemberain/langohr "3.2.0"]
                  ]
   
   :profiles {:dev {:plugins [
--- src/edeposit/clj_amqp/reactive.clj
+++ src/edeposit/clj_amqp/reactive.clj
@@ -1,5 +1,6 @@
 (ns edeposit.clj-amqp.reactive
   (:require    [rx.lang.clojure.core :as rx]
+               [langohr.basic :as lb]
                )
   (:import  [rx Observable]
             [java.util.concurrent TimeUnit]
@@ -12,4 +13,13 @@
        (rx/map fn)
        (rx/filter (complement nil?))
       )
+  )
+
+(defn queue->obs 
+  ([channel queue-name interval time-unit]
+   (make-queue-obs (fn [val] (lb/get channel queue-name)) interval time-unit)  
+   )
+  ([channel queue-name]
+   (make-queue-obs (fn [val] (lb/get channel queue-name)) 500 TimeUnit/MILLISECONDS)  
+   )
   )
